0b016ef4c368890936c4bf1fe1e3440aef1f8e8a
nsankethreddy <sankethn1947@gmail.com>
2020-07-31 01:51:11 +0530

    indented code

--- client.py
+++ client.py
@@ -3,23 +3,23 @@ import threading
 import base64
 
 passkey = input("Enter your accesskey: ")
-PORT= 5050
+PORT = 5050
 SERVER = decode_key(passkey)
-ADDR = (SERVER,PORT)
+ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 HEADER = 64
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#connect to the socket
+# connect to the socket
 client.connect(ADDR)
 
 def decode_key(valu):
     decoded_data = base64.b64decode(valu)
     dec_ip = decoded_data.decode('utf-8')
     if len(dec_ip) == 7:
-        dec_ip = '192.168.'+ dec_ip.lstrip('0')
+        dec_ip = '192.168.' + dec_ip.lstrip('0')
     elif len(dec_ip) == 15:
         dec_ip = dec_ip.lstrip('0')
     else:
@@ -30,18 +30,16 @@ def send(message_val):
     message = message_val.encode(FORMAT)
     message_length = len(message)
     send_len = str(message_length).encode(FORMAT)
-    #padd it to header
-    send_len+= b' '*(HEADER-len(send_len))
+    # padd it to header
+    send_len += b' '*(HEADER-len(send_len))
     client.send(send_len)
     client.send(message)
     print(client.recv(1024).decode(FORMAT))
-
 
 send("Hello")
 send("Peter Kavinsky")
 send("Jacob Elordi")
 send("aaa")
 send("ooo")
-#Disconnect
+# Disconnect
 send("!DISCONNECT")
-
--- server.py
+++ server.py
@@ -14,12 +14,12 @@ else:
     print('Unsupported OS')
     os.exit(1)
 
-PORT= 5050
+PORT = 5050
 
 def encodefunc(val):
-    encoded_data = base64.b64encode(bytes(val,'utf-8'))
+    encoded_data = base64.b64encode(bytes(val, 'utf-8'))
     print(f"\n\n\t\t\t\t--------      Your access code : ( {encoded_data.decode('utf-8')} )      --------")
-   
+
 def getpasskey(str1):
     if str1[0:8] == '192.168.':
         encodefunc(str1[8:].zfill(7))
@@ -28,44 +28,38 @@ def getpasskey(str1):
 
 getpasskey(SERVER)
 
-ADDR =(SERVER,PORT)
-FORMAT='utf-8'
-#header of 64 bytes : tells us the length of the message coming
-HEADER=64
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+# header of 64 bytes : tells us the length of the message coming
+HEADER = 64
 DISCONNECT_MESSAGE = "!DISCONNECT"
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
-#handle all the communications
-def handle_client(conn,addr):
+def handle_client(conn, addr):
     print(f"[New connection from client] {addr}")
     connected = True
     while(connected):
-        #message=conn.recv(1024)
-        message_length=conn.recv(HEADER).decode(FORMAT)
+        # message=conn.recv(1024)
+        message_length = conn.recv(HEADER).decode(FORMAT)
         if message_length:
             message_length = int(message_length)
             message = conn.recv(message_length).decode(FORMAT)
-            if message == DISCONNECT_MESSAGE :
+            if message == DISCONNECT_MESSAGE:
                 connected = False
             print(f"[{addr}] {message}")
             conn.send('Received'.encode(FORMAT))
-        
-    
+
     conn.close()
 
-
 def start_sockets():
-    #handle only the incoming connections , ind clients
     server.listen()
     print(f"[LISTENING ON : {SERVER}]")
     while(1):
-        #waits for a new connection 
-        conn,addr = server.accept()
-        thread1 = threading.Thread(target = handle_client,args = (conn,addr))
+        conn, addr = server.accept()
+        thread1 = threading.Thread(target=handle_client, args=(conn, addr))
         thread1.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}")
-        
 
 print('Starting server')
 start_sockets()
