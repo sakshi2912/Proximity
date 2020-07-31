87f1e2f38c6bc03cb804794e9d2e9abe894d77d5
sakshi2912 <sakshiv700@gmail.com>
2020-07-31 11:52:57 +0530

    Add Try except block for incorrect key

--- client.py
+++ client.py
@@ -4,27 +4,31 @@ import base64
 
 passkey = input("Enter your accesskey: ")
 PORT = 5050
-SERVER = decode_key(passkey)
-ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 HEADER = 64
 DISCONNECT_MESSAGE = "!DISCONNECT"
-
+# connect to the socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# connect to the socket
-client.connect(ADDR)
-
 def decode_key(valu):
-    decoded_data = base64.b64decode(valu)
-    dec_ip = decoded_data.decode('utf-8')
-    if len(dec_ip) == 7:
-        dec_ip = '192.168.' + dec_ip.lstrip('0')
-    elif len(dec_ip) == 15:
-        dec_ip = dec_ip.lstrip('0')
-    else:
-        print("Incorrect access key")
-    return dec_ip
+    try:
+        decoded_data = base64.b64decode(valu)
+        dec_ip = decoded_data.decode('utf-8')
+        if len(dec_ip) == 7:
+            dec_ip = '192.168.' + dec_ip.lstrip('0')
+        elif len(dec_ip) == 15:
+            dec_ip = dec_ip.lstrip('0')
+    except:
+        print(" Incorrect access key ")
+        passkey = input(" Re-enter your accesskey : ")
+        dec_ip = decode_key(passkey)
+    
+    finally:
+         return dec_ip
+     
+SERVER = decode_key(passkey)
+ADDR = (SERVER, PORT)
+client.connect(ADDR)
 
 def send(message_val):
     message = message_val.encode(FORMAT)
