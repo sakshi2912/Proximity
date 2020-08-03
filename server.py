7f37e8b02560e73971690a575a3a4b92dc7ae0cd
root <root@kali>
2020-08-03 19:19:43 +0530

    Add queue to discard pre-text

--- server.py
+++ server.py
@@ -10,6 +10,7 @@ from termcolor import cprint
 import sys
 from subprocess import check_output
 import time
+import queue
 
 if platform == "linux" or platform == "linux2":
     os.system('clear')
@@ -49,7 +50,7 @@ def getpasskey(str1):
     else:
         encodefunc(str1.zfill(15))
 
-
+no_client = 1
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 # header of 64 bytes : tells us the length of the message coming
@@ -63,10 +64,12 @@ except:
     print('A room already exists in this server')
     exit(1)
 
+cache_msg = queue.Queue()
 
 def readinput():
     global user_input
     user_input = input()
+    cache_msg.put(user_input)
     return
 
 
@@ -76,32 +79,33 @@ connection_cl = 0
 
 def handle_client(conn, addr):
     try:
+        global no_client
+        
         global userinput
+        global connection_cl
         uname = conn.recv(10).decode(FORMAT)
         #print(f"\n[New connection from {addr[0]}]")
         print(f"{uname} joined the chat")
-        connected = True
-        while(connected):
+        no_client = 0
+        print(no_client,"Send message")
+        #cache_msg.queue.clear()
+        while(True):
             message_length = conn.recv(HEADER).decode(FORMAT)
-            if message_length:
-                message_length = int(message_length)
-                message = conn.recv(message_length).decode(FORMAT)
-                if message == DISCONNECT_MESSAGE:
-                    print(f"\t\t\t\t\t\t{uname} > {message}")
-                    print(f'{uname} left the chat')
-                    connected = False
-                    global connection_cl
-                    connection_cl = 1
-                    userinput.join()
-                    conn.close()
-                    return
-                print(f"\t\t\t\t\t\t{uname} > {message}")
+            message_length = int(message_length)
+            message_rec = conn.recv(message_length).decode(FORMAT)
+            if message_rec == 'exit':
+                print(f'{uname} left the chat')
+                no_client = 1
+                connection_cl = 1
+                print(no_client , "No messsage to send")
+                userinput.join()
+                #conn.close()
+                break
+            print(f"\t\t\t\t\t\t{uname} > {message_rec}")
         conn.close()
-        return
-    except (ConnectionResetError, ConnectionAbortedError):
-        print('The connection is closed , you must restart the terminal')
-    except OSError:
-        print('There was some problem connecting you to the chat, please try again in some time')
+        
+    except (ConnectionResetError, ConnectionAbortedError,OSError):
+        pass
 
 
 user_input = ''
@@ -112,34 +116,58 @@ def send_message(conn, addr):
     global userinput
     global connection_cl
     global message_val
+    global cache_msg
+    global no_client
+    cache_msg.queue.clear()
+    #no_client = 0
     while(conn.fileno()):
-        if not userinput.is_alive():
+        
+        if (not userinput.is_alive()):
             global user_input
+            
             if user_input != '':
-                message_val = user_input
-                another = user_input
+                
+                if no_client == 1:
+                    
+                    cache_msg.queue.clear()
+                    
+                    cache_msg.put(username)        
+                
+                
+                message_val = cache_msg.get()
+                print(no_client,"--")
+                print(cache_msg.qsize())
+                another = message_val
             user_input = ''
+            
             userinput = threading.Thread(target=readinput, args=())
             userinput.start()
-        if message_val != '':
-            message = message_val.encode(FORMAT)
-            message_val = ''
-            message_length = len(message)
-            send_len = str(message_length).encode(FORMAT)
-            send_len += b' '*(HEADER-len(send_len))
-            try:
-                conn.send(send_len)
-                conn.send(message)
-                if another == DISCONNECT_MESSAGE:
-                    conn.close()
-                    os._exit(0)
-            except:
-                print('Connection closed')
-                conn.close()
-                return
+        if message_val != '' :
+            
+
+            if no_client == 0:
+                print("Sent")
+                message = message_val.encode(FORMAT)
+                message_val = ''
+                message_length = len(message)
+                send_len = str(message_length).encode(FORMAT)
+                send_len += b' '*(HEADER-len(send_len))
+                try:
+                    conn.send(send_len)
+                    conn.send(message)
+                    if another == 'exit':
+                        conn.close()
+                        cache_msg.queue.clear()
+                        os._exit(0)
+                except:
+                    #print('Connection closed')
+                    #conn.close()
+                    #return
+                    pass
         if connection_cl == 1:
             connection_cl = 0
             conn.close()
+            cache_msg.queue.clear()
             return
     conn.close()
     os._exit(0)
