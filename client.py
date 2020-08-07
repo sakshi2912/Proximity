2eca09d657986e42e201075b74aaf5336885de5a
nsankethreddy <sankethn1947@gmail.com>
2020-08-07 23:44:35 +0530

    Catching Kill signals and redirecting it to Disconnect messages

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 import os
+import signal
+
 
 username = input("Enter your username: ")
 
@@ -15,36 +17,55 @@ def receive():
             message = client.recv(1024).decode('utf-8')
             if message == 'Connect':
                 client.send(username.encode('utf-8'))
-            
+
             elif message == 'Server left':
                 print('\nServer has disconnected\n')
                 os._exit(0)
 
             elif message[-5:] == 'left!':
-                print('\n \t',messge,'\n')
+                print('\n \t', messge, '\n')
 
             elif message == 'Connected to server!':
                 print('\n \t Connected to the Server! \n')
 
             else:
-                print('\t\t\t\t',message)
+                print('\t\t\t\t', message)
         except:
             print("An error occured!")
             client.close()
             break
-        
+
+
 def write():
     while True:
-        input_val = input()
-        if input_val == 'exit':
+        try:
+            input_val = input()
+            if input_val == 'exit':
+                client.send('exit'.encode('utf-8'))
+                client.close()
+                print('You will be disconnected')
+                os._exit(0)
+            else:
+                message = f'[{username}] : {input_val}'
+                client.send(message.encode('utf-8'))
+        except:
+            print('\n \t Error Occoured while Reading input \n')
             client.send('exit'.encode('utf-8'))
             client.close()
             print('You will be disconnected')
             os._exit(0)
-        else:
-            message = f'[{username}] : {input_val}'
-            client.send(message.encode('utf-8'))
-        
+
+
+def keyboardInterruptHandler(signal, frame):
+    print('Interrupted')
+    client.send('exit'.encode('utf-8'))
+    client.close()
+    print('You will be disconnected')
+    os._exit(0)
+
+
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 import os
+import signal
+
 host = '127.0.0.1'
 port = 55455
 
@@ -11,15 +13,17 @@ server.listen()
 
 clients_dict = {}
 
-def broadcast(message,current_client):
-    send_client_list = [x for x in list(clients_dict.keys()) if x!= current_client and   x!='Server']
+
+def broadcast(message, current_client):
+    send_client_list = [x for x in list(
+        clients_dict.keys()) if x != current_client and x != 'Server']
     for client in send_client_list:
         try:
             client.send(message)
         except:
             print('\n \t Could not send message \n')
 
-        
+
 def rec_message(client):
     while True:
         try:
@@ -29,48 +33,63 @@ def rec_message(client):
                 print(f'\n \t [{user}] disconnected \n')
                 del clients_dict[client]
                 client.close()
-                broadcast(f'\n \t [{user}] left! \n'.encode('utf-8'),client)
+                broadcast(f'\n \t [{user}] left! \n'.encode('utf-8'), client)
                 break
-            
+
             else:
-                print('\t\t\t\t',message)
-                broadcast(message.encode('utf-8'),client)
-            
+                print('\t\t\t\t', message)
+                broadcast(message.encode('utf-8'), client)
+
         except:
             user = clients_dict[client]
             print(f'\n \t [{user}] disconnected \n')
             client.close()
             del clients_dict[client]
-            broadcast(f'\n \t [{user}] left! \n'.encode('utf-8'),client)
+            broadcast(f'\n \t [{user}] left! \n'.encode('utf-8'), client)
             break
-        
+
+
 def send_message():
     while True:
         try:
             message = input()
-            b_message = f"[{server_name}] : {message}"    
+            b_message = f"[{server_name}] : {message}"
             if message == 'exit':
-                broadcast('Server left'.encode('utf-8'),'Server')
+                broadcast('Server left'.encode('utf-8'), 'Server')
                 os._exit(0)
-            broadcast(b_message.encode('utf-8'),'Server')
-            
+            broadcast(b_message.encode('utf-8'), 'Server')
+
         except:
-            break
-        
+            print('\n \t Error Occoured while Reading input \n')
+            broadcast('Server left'.encode('utf-8'), 'Server')
+            os._exit(0)
+
+
+def keyboardInterruptHandler(signal, frame):
+    print('Interrupted')
+    broadcast('Server left'.encode('utf-8'), 'Server')
+    os._exit(0)
+
+
+signal.signal(signal.SIGINT, keyboardInterruptHandler)
+
+
 def accept_conn():
     while True:
-        
+
         client, address = server.accept()
 
         client.send('Connect'.encode('utf-8'))
         clients_dict[client] = client.recv(1024).decode('utf-8')
         print(f"\n \t [{clients_dict[client]}] joined the server \n")
-        broadcast(f"\n \t [{clients_dict[client]}] joined! \n".encode('utf-8'),client)
+        broadcast(f"\n \t [{clients_dict[client]}] joined! \n".encode(
+            'utf-8'), client)
         client.send('Connected to server!'.encode('utf-8'))
 
         thread = threading.Thread(target=rec_message, args=(client,))
         thread.start()
-        print('Active threads : '+ str(threading.active_count()-1))
+        print('Active threads : ' + str(threading.active_count()-1))
+
 
 print('Creating server')
 server_name = input('Enter server name : ')
@@ -79,4 +98,3 @@ thread2 = threading.Thread(target=send_message)
 thread2.start()
 print('Created server')
 accept_conn()
-
