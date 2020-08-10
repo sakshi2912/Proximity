7e5535a73d76b3efd63604ff9e270a0e9d11e6db
sakshi2912 <sakshiv700@gmail.com>
2020-08-10 22:35:16 +0530

    Layout 1 : asciimatics

--- menu1.py
+++ menu1.py
@@ -1,50 +1,119 @@
-import urwid
-import os 
-from pyfiglet import figlet_format
-import pyfiglet
+from random import randint
+from asciimatics.screen import Screen
+from asciimatics.widgets import (
+    Frame,
+    ListBox,
+    Layout,
+    Divider,
+    Text,
+    Button,
+    TextBox,
+    Widget,
+    PopUpDialog,
+)
+from asciimatics.scene import Scene
+from asciimatics.screen import Screen
+from asciimatics.exceptions import ResizeScreenError, NextScene, StopApplication
+import os
 
-choices = ["Create Server","Join Server"]
-
-def menu(title, choices):
-    body = [urwid.Text(title), urwid.Divider()]
-    for c in choices:
-        button = urwid.Button(c)
-        urwid.connect_signal(button, 'click', item_chosen, c)
-        body.append(urwid.AttrMap(button, None, focus_map='reversed'))
-    return urwid.ListBox(urwid.SimpleFocusListWalker(body))
-
-def item_chosen(button, choice):
-    
-    done = urwid.Button(u'Ok')
-    if choice == 'Create Server':
-        response = urwid.Text([u'Creating Server ', u'\n'])
-        my_name = urwid.Edit('Enter a username \n')
-
-        urwid.connect_signal(done, 'click', start_server)
-
-    else:
-        response = urwid.Text([u'Joining Server ', u'\n'])
-        urwid.connect_signal(done, 'click', join_server)
-    main.original_widget = urwid.Filler(urwid.Pile([response,urwid.AttrMap(done, None, focus_map='reversed')]))
-
-def start_server(button):
-    os.system("python3 server.py")
-
-def join_server(button):
-    os.system("python3 clientui.py")
-    raise urwid.ExitMainLoop()
-    #os.system("python3 client.py")
+form_data = {
+    "TB": "Value1",
+}
 
 
-welcomeText = pyfiglet.figlet_format('Proximity')
-txt = urwid.Text(welcomeText,'center')
-fill = urwid.Filler(txt, 'top')
-main = urwid.Padding(menu(u'Menu', choices), left=2, right=2)
-top = urwid.Overlay(main, urwid.SolidFill(u'\N{MEDIUM SHADE}'),
-    align='center', width=('relative', 60),
-    valign='middle', height=('relative', 60),
-    min_width=20, min_height=9)
-top2 = urwid.Pile([fill,top])
-urwid.MainLoop(top2, palette=[('reversed', 'standout', '')]).run()
-#loop = urwid.MainLoop(fill)
-#loop.run()
+class myclass(Frame):
+    def __init__(self, screen):
+        super(myclass, self).__init__(
+            screen,
+            screen.height * 2 // 3,
+            screen.width * 2 // 3,
+            data=form_data,
+            hover_focus=True,
+            title="PROXIMITY",
+        )
+        layout = Layout([1, 1, 1])
+        self.add_layout(layout)
+        layout.add_widget(Button("Create server", self._createserver), 0)
+        layout.add_widget(Button("Join server", self._view), 1)
+        layout.add_widget(Button("Quit", self._quit), 2)
+        self.fix()
+
+    def _view(self):
+        self.save()
+        self._scene.add_effect(PopUpDialog(self._screen, "Hello", ["OK"]))
+
+    def _createserver(self):
+        self.save()
+        os.system("python3 server.py")
+        raise NextScene("menu2")
+
+    def _quit(self):
+        self._scene.add_effect(
+            PopUpDialog(
+                self._screen, "Are you sure?", ["Yes", "No"], on_close=self._quit_on_yes
+            )
+        )
+
+    @staticmethod
+    def _quit_on_yes(selected):
+        # Yes is the first button
+        if selected == 0:
+            raise StopApplication("User requested exit")
+
+
+class myclass2(Frame):
+    def __init__(self, screen):
+        super(myclass2, self).__init__(
+            screen,
+            screen.height * 2 // 3,
+            screen.width * 2 // 3,
+            data=form_data,
+            hover_focus=True,
+            title="PROXIMITY",
+        )
+        layout = Layout([1, 1, 1])
+        self.add_layout(layout)
+        layout.add_widget(
+            Text(label="Enter username", name="TB", on_change=self._on_change), 0
+        )
+        layout.add_widget(Button("View", self._view), 1)
+        layout.add_widget(Button("Quit", self._quit), 2)
+        self.fix()
+
+    def _on_change(self):
+        global form_data
+        self.save()
+        for key, value in self.data.items():
+            form_data[key] = value
+
+    def _view(self):
+        global form_data
+        self.save()
+        message = "Hi , {}".format(form_data["TB"])
+        self._scene.add_effect(PopUpDialog(self._screen, message, ["OK"]))
+
+    def _quit(self):
+        self._scene.add_effect(
+            PopUpDialog(
+                self._screen, "Are you sure?", ["Yes", "No"], on_close=self._quit_on_yes
+            )
+        )
+
+    @staticmethod
+    def _quit_on_yes(selected):
+        # Yes is the first button
+        if selected == 0:
+            raise StopApplication("User requested exit")
+
+
+def demo(screen):
+
+    scenes = [
+        Scene([myclass(screen)], -1, name="menu1"),
+        Scene([myclass2(screen)], -1, name="menu2"),
+    ]
+    screen.play(scenes, stop_on_resize=True)
+
+
+Screen.wrapper(demo)
+
