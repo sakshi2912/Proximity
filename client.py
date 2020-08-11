022b302205156f1a98b3ae93e38e1f663656439b
nsankethreddy <sankethn1947@gmail.com>
2020-08-11 21:41:00 +0530

    Moved all the functions to classes

--- client.py
+++ client.py
@@ -7,6 +7,90 @@ import sys
 import base64
 
 
+class clientType:
+
+    def decode_key(self, valu):
+        try:
+            decoded_data = base64.b64decode(valu)
+            dec_ip = decoded_data.decode('utf-8')
+
+            if len(dec_ip) == 8:
+                dec_ip = '192.168' + dec_ip.lstrip('0')
+            elif len(dec_ip) == 15:
+                dec_ip = dec_ip.lstrip('0')
+            elif len(dec_ip) == 0:
+                print("Please enter a passkey \n ")
+                passkey = input(" Re-enter your accesskey : ")
+                dec_ip = self.decode_key(passkey)
+            else:
+                print("Please enter the correct passkey \n ")
+                passkey = input(" Re-enter your accesskey : ")
+                dec_ip = self.decode_key(passkey)
+
+        except (ConnectionRefusedError, UnicodeDecodeError, UnboundLocalError, base64.binascii.Error):
+            print("Please enter the correct passkey \n ")
+            passkey = input(" Re-enter your accesskey : ")
+            dec_ip = self.decode_key(passkey)
+
+        finally:
+            return dec_ip
+
+    def receive(self):
+        global username
+        while True:
+            try:
+
+                message = client.recv(1024).decode('utf-8')
+                if message == 'Connect':
+                    client.send(username.encode('utf-8'))
+
+                elif message == 'Server left':
+                    print('\nServer has disconnected\n')
+                    os._exit(0)
+
+                elif 'Connected to' in message:
+                    print('\n \t ', message, '\n')
+
+                elif 'Username updated to [' in message:
+                    print(message)
+                    username = message[25:-1]
+
+                else:
+                    print('\t\t\t\t', message)
+            except:
+                print("An error occured!")
+                client.close()
+                break
+
+    def write(self):
+        while True:
+            try:
+                input_val = input()
+                if input_val == DISCONNECT_MESSAGE:
+                    client.send(DISCONNECT_MESSAGE.encode('utf-8'))
+                    client.close()
+                    print('You will be disconnected')
+                    os._exit(0)
+                else:
+                    message = f'[{username}] : {input_val}'
+                    client.send(message.encode('utf-8'))
+            except:
+                print('\n \t Error Occoured while Reading input \n')
+                client.send(DISCONNECT_MESSAGE.encode('utf-8'))
+                client.close()
+                print('You will be disconnected')
+                os._exit(0)
+
+    def keyboardInterruptHandler(self, signal, frame):
+        print('Interrupted')
+        client.send(DISCONNECT_MESSAGE.encode('utf-8'))
+        client.close()
+        print('You will be disconnected')
+        os._exit(0)
+
+
+c1 = clientType()
+
 if platform == "linux" or platform == "linux2":
     os.system('clear')
 elif platform == "win32":
@@ -18,106 +102,21 @@ else:
 passkey = sys.argv[1]
 PORT = 5050
 DISCONNECT_MESSAGE = "exit"
-
-
-def decode_key(valu):
-    try:
-        decoded_data = base64.b64decode(valu)
-        dec_ip = decoded_data.decode('utf-8')
-
-        if len(dec_ip) == 8:
-            dec_ip = '192.168' + dec_ip.lstrip('0')
-        elif len(dec_ip) == 15:
-            dec_ip = dec_ip.lstrip('0')
-        elif len(dec_ip) == 0:
-            print("Please enter a passkey \n ")
-            passkey = input(" Re-enter your accesskey : ")
-            dec_ip = decode_key(passkey)
-        else:
-            print("Please enter the correct passkey \n ")
-            passkey = input(" Re-enter your accesskey : ")
-            dec_ip = decode_key(passkey)
-
-    except (ConnectionRefusedError, UnicodeDecodeError, UnboundLocalError, base64.binascii.Error):
-        print("Please enter the correct passkey \n ")
-        passkey = input(" Re-enter your accesskey : ")
-        dec_ip = decode_key(passkey)
-
-    finally:
-        return dec_ip
-
-
-IP = decode_key(passkey)
+IP = c1.decode_key(passkey)
 
 username = input("Enter your username: ")
 while not username.isalpha():
-        print(" \n \t ERROR: The username should only contain alphabates. \n")
-        username = input('Enter server name : ')
+    print(" \n \t ERROR: The username should only contain alphabates. \n")
+    username = input('Enter server name : ')
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((IP, PORT))
 
 
-def receive():
-    global username
-    while True:
-        try:
+signal.signal(signal.SIGINT, c1.keyboardInterruptHandler)
 
-            message = client.recv(1024).decode('utf-8')
-            if message == 'Connect':
-                client.send(username.encode('utf-8'))
-
-            elif message == 'Server left':
-                print('\nServer has disconnected\n')
-                os._exit(0)
-
-            elif 'Connected to' in message:
-                print('\n \t ',message,'\n')
-
-            elif 'Username updated to [' in message:
-                print(message)   
-                username = message[25:-1]
-
-            else:
-                print('\t\t\t\t', message)
-        except:
-            print("An error occured!")
-            client.close()
-            break
-
-
-def write():
-    while True:
-        try:
-            input_val = input()
-            if input_val == DISCONNECT_MESSAGE:
-                client.send(DISCONNECT_MESSAGE.encode('utf-8'))
-                client.close()
-                print('You will be disconnected')
-                os._exit(0)
-            else:
-                message = f'[{username}] : {input_val}'
-                client.send(message.encode('utf-8'))
-        except:
-            print('\n \t Error Occoured while Reading input \n')
-            client.send(DISCONNECT_MESSAGE.encode('utf-8'))
-            client.close()
-            print('You will be disconnected')
-            os._exit(0)
-
-
-def keyboardInterruptHandler(signal, frame):
-    print('Interrupted')
-    client.send(DISCONNECT_MESSAGE.encode('utf-8'))
-    client.close()
-    print('You will be disconnected')
-    os._exit(0)
-
-
-signal.signal(signal.SIGINT, keyboardInterruptHandler)
-
-receive_thread = threading.Thread(target=receive)
+receive_thread = threading.Thread(target=c1.receive)
 receive_thread.start()
 
-write_thread = threading.Thread(target=write)
+write_thread = threading.Thread(target=c1.write)
 write_thread.start()
--- server.py
+++ server.py
@@ -6,6 +6,104 @@ from sys import platform
 import base64
 
 
+class serverType:
+
+    def encodefunc(self, val):
+        encoded_data = base64.b64encode(bytes(val, 'utf-8'))
+        print(
+            f"\n\n-------- {server_name}'s Chat-Room accesskey : ( {encoded_data.decode('utf-8')} ) --------")
+
+    def getpasskey(self, str1):
+        if str1[0:7] == '192.168':
+            self.encodefunc(str1[7:].zfill(8))
+        else:
+            self.encodefunc(str1.zfill(15))
+
+    def broadcast(self, message, current_client):
+        send_client_list = [x for x in list(
+            clients_dict.keys()) if x != current_client and x != 'Server']
+        for client in send_client_list:
+            try:
+                client.send(message)
+            except:
+                print('\n \t Could not send message \n')
+
+    def rec_message(self, client):
+        while True:
+            try:
+                message = client.recv(1024).decode('utf-8')
+                if message == DISCONNECT_MESSAGE:
+                    user = clients_dict[client]
+                    print(f'\n \t [{user}] disconnected \n')
+                    del clients_dict[client]
+                    client.close()
+                    self.broadcast(
+                        f'\n \t [{user}] left! \n'.encode('utf-8'), client)
+                    break
+
+                else:
+                    print('\t\t\t\t', message)
+                    self.broadcast(message.encode('utf-8'), client)
+
+            except:
+                user = clients_dict[client]
+                print(f'\n \t [{user}] disconnected \n')
+                client.close()
+                del clients_dict[client]
+                self.broadcast(
+                    f'\n \t [{user}] left! \n'.encode('utf-8'), client)
+                break
+
+    def send_message(self):
+        while True:
+            try:
+                message = input()
+                b_message = f"[{server_name}] : {message}"
+                if message == DISCONNECT_MESSAGE:
+                    self.broadcast('Server left'.encode('utf-8'), 'Server')
+                    os._exit(0)
+                self.broadcast(b_message.encode('utf-8'), 'Server')
+
+            except:
+                print('\n \t Error Occoured while Reading input \n')
+                self.broadcast('Server left'.encode('utf-8'), 'Server')
+                os._exit(0)
+
+    def accept_conn(self):
+        while True:
+
+            client, address = server.accept()
+
+            client.send('Connect'.encode('utf-8'))
+            client_name = client.recv(1024).decode('utf-8')
+            final_client_name = client_name
+            i = 1
+            while final_client_name in clients_dict.values():
+                final_client_name = f"{client_name}{i}"
+                i += 1
+
+            if client_name != final_client_name:
+                message = (
+                    '\n \t Username updated to ['+final_client_name+']').encode('utf-8')
+                client.send(message)
+            clients_dict[client] = final_client_name
+            print(f"\n \t [{clients_dict[client]}] joined the server \n")
+            self.broadcast(f"\n \t [{clients_dict[client]}] joined! \n".encode(
+                'utf-8'), client)
+            client.send(f"Connected to [{server_name}]!".encode('utf-8'))
+
+            thread = threading.Thread(target=self.rec_message, args=(client,))
+            thread.start()
+            print('Active threads : ' + str(threading.active_count()-1))
+
+    def keyboardInterruptHandler(self, signal, frame):
+        print('Interrupted')
+        self.broadcast('Server left'.encode('utf-8'), 'Server')
+        os._exit(0)
+
+
+s1 = serverType()
+
 if platform == "linux" or platform == "linux2":
     os.system('clear')
     if (os.path.exists('ip.txt')):
@@ -22,129 +120,24 @@ else:
     print('Unsupported OS')
     exit(1)
 
-clients_dict = {}
 print('Creating server')
 server_name = input('Enter server name : ')
 while not server_name.isalpha():
     print(" \n \t ERROR: The Server name should only contain a set of alphabates. \n")
     server_name = input('Enter server name : ')
 
-
+clients_dict = {}
 clients_dict['Server'] = server_name
-
 PORT = 5050
-
-
-def encodefunc(val):
-    encoded_data = base64.b64encode(bytes(val, 'utf-8'))
-    print(
-        f"\n\n-------- {server_name}'s Chat-Room accesskey : ( {encoded_data.decode('utf-8')} ) --------")
-
-
-def getpasskey(str1):
-    if str1[0:7] == '192.168':
-        encodefunc(str1[7:].zfill(8))
-    else:
-        encodefunc(str1.zfill(15))
-
-
-getpasskey(IP)
-
+DISCONNECT_MESSAGE = "exit"
+s1.getpasskey(IP)
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((IP, PORT))
 server.listen()
 
-DISCONNECT_MESSAGE = "exit"
+signal.signal(signal.SIGINT, s1.keyboardInterruptHandler)
 
-
-def broadcast(message, current_client):
-    send_client_list = [x for x in list(
-        clients_dict.keys()) if x != current_client and x != 'Server']
-    for client in send_client_list:
-        try:
-            client.send(message)
-        except:
-            print('\n \t Could not send message \n')
-
-
-def rec_message(client):
-    while True:
-        try:
-            message = client.recv(1024).decode('utf-8')
-            if message == DISCONNECT_MESSAGE:
-                user = clients_dict[client]
-                print(f'\n \t [{user}] disconnected \n')
-                del clients_dict[client]
-                client.close()
-                broadcast(f'\n \t [{user}] left! \n'.encode('utf-8'), client)
-                break
-
-            else:
-                print('\t\t\t\t', message)
-                broadcast(message.encode('utf-8'), client)
-
-        except:
-            user = clients_dict[client]
-            print(f'\n \t [{user}] disconnected \n')
-            client.close()
-            del clients_dict[client]
-            broadcast(f'\n \t [{user}] left! \n'.encode('utf-8'), client)
-            break
-
-
-def send_message():
-    while True:
-        try:
-            message = input()
-            b_message = f"[{server_name}] : {message}"
-            if message == DISCONNECT_MESSAGE:
-                broadcast('Server left'.encode('utf-8'), 'Server')
-                os._exit(0)
-            broadcast(b_message.encode('utf-8'), 'Server')
-
-        except:
-            print('\n \t Error Occoured while Reading input \n')
-            broadcast('Server left'.encode('utf-8'), 'Server')
-            os._exit(0)
-
-
-def keyboardInterruptHandler(signal, frame):
-    print('Interrupted')
-    broadcast('Server left'.encode('utf-8'), 'Server')
-    os._exit(0)
-
-
-signal.signal(signal.SIGINT, keyboardInterruptHandler)
-
-
-def accept_conn():
-    while True:
-
-        client, address = server.accept()
-
-        client.send('Connect'.encode('utf-8'))
-        client_name = client.recv(1024).decode('utf-8')
-        final_client_name = client_name
-        i = 1
-        while final_client_name in clients_dict.values():
-            final_client_name = f"{client_name}{i}"
-            i += 1
-
-        if client_name != final_client_name:
-            message = ('\n \t Username updated to ['+final_client_name+']').encode('utf-8')
-            client.send(message)
-        clients_dict[client] = final_client_name
-        print(f"\n \t [{clients_dict[client]}] joined the server \n")
-        broadcast(f"\n \t [{clients_dict[client]}] joined! \n".encode(
-            'utf-8'), client)
-        client.send(f"Connected to [{server_name}]!".encode('utf-8'))
-
-        thread = threading.Thread(target=rec_message, args=(client,))
-        thread.start()
-        print('Active threads : ' + str(threading.active_count()-1))
-
-
-thread2 = threading.Thread(target=send_message)
+thread2 = threading.Thread(target=s1.send_message)
 thread2.start()
 print('Created server')
-accept_conn()
+s1.accept_conn()
